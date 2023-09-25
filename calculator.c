7c68df4df4c19dc0b1f920b42c0e183d6cea33cf
Leanstix <leanstixx@gmail.com>
2023-09-25 14:05:46 +0100

    commit

--- calculator.c
+++ calculator.c
@@ -12,6 +12,7 @@ int main(void)
 	char g[] = "volume given only pressure(1&2)";
 	char h[] = "Entropy";
 	char s;
+	char y[3];
 	double A;
 	double B;
 	double C;
@@ -23,16 +24,26 @@ int main(void)
 
 	printf("what Thermodynamic operation would you like to perform? ");
 	printf(" \na = %s \nb = %s \nc = %s \nd = %s \ne = %s \nf = %s \ng = %s \nh = %s\n", a, b, c, d, e, f, g, h);
-	scanf("%c");
-	if(s = a)
+	scanf("%c", &s);
+	if(s =='a')
 	{
-		printf("the chosen operation is \" Gas Law \" ");
+		printf("\nthe chosen operation is \" Gas Law \" ");
+		fgets("",3 ,stdin);
+		if ()
 	}
-	else if (s = b)
+	else if (s =='b')
 	{
 		printf("The chosen operation to be performed is \" Enthalpy \"");
 	}
-	else 
+	else if (s == 'c')
+	{
+		printf("The chosen operation to be performed is \"Gibbs free energy\"")
+	}
+	else if (s == 'd')
+	 {
+		printf("The chosen operation to be performed is \"vant hoff equation\"")
+	 }
+	else
 	{
 		printf("go and eat");
 	}
